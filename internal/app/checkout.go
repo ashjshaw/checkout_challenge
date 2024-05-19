0cb20fb644d0cae860253106db8fa8dbd37b806c
ashjshaw <ashshaw17@gmail.com>
2024-05-19 19:02:52 +0100

    priceList populated and SKU struct created

--- internal/app/checkout.go
+++ internal/app/checkout.go
@@ -2,7 +2,12 @@ package checkout
 
 type Handler struct{}
 
-type SKU struct{}
+type SKU struct {
+	Identifier           string `json:"identifier"`
+	UnitPrice            int    `json:"unitPrice"`
+	SpecialPriceQuantity int    `json:"specialPriceQuantity"`
+	SpecialPrice         int    `json:"specialPrice"`
+}
 
 func (i *Handler) Checkout() error {
 	panic("NYI")
