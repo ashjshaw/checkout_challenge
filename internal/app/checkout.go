ff978b05d2122917cdcf82079e168c91f24a8a50
ashjshaw <ashshaw17@gmail.com>
2024-05-19 19:04:50 +0100

    Populated Handler struct

--- internal/app/checkout.go
+++ internal/app/checkout.go
@@ -1,6 +1,10 @@
 package checkout
 
-type Handler struct{}
+type Handler struct {
+	ReadFile  func(string) ([]byte, error)
+	Scanln    func(...any) (int, error)
+	Unmarshal func([]byte, any) error
+}
 
 type SKU struct {
 	Identifier           string `json:"identifier"`
@@ -13,10 +17,10 @@ func (i *Handler) Checkout() error {
 	panic("NYI")
 }
 
-func createPrices() ([]SKU, error) {
+func (i *Handler) createPrices() ([]SKU, error) {
 	panic("NYI")
 }
 
-func calculateTotal(skuPriceList []SKU, itemList string) int {
+func (i *Handler) calculateTotal(skuPriceList []SKU, itemList string) int {
 	panic("NYI")
 }
