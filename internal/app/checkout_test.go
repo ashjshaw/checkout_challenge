950f5e11b8a88281500eb988769de88601db8c1e
ashjshaw <ashshaw17@gmail.com>
2024-05-19 19:31:46 +0100

    createPrices function, ReadFile returns no error test created

--- internal/app/checkout_test.go
+++ internal/app/checkout_test.go
@@ -1,6 +1,9 @@
 package checkout
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_calculateTotal(t *testing.T) {
 	type args struct {
@@ -78,6 +81,44 @@ func Test_calculateTotal(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := calculateTotal(tt.args.skuPriceList, tt.args.itemList); got != tt.want {
 				t.Errorf("calculateTotal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHandler_createPrices(t *testing.T) {
+	tests := []struct {
+		name    string
+		i       *Handler
+		want    []SKU
+		wantErr bool
+	}{
+		{
+			name: "When given a valid priceList.json, no error is returned from the ReadFile function",
+			i: &Handler{ReadFile: func(s string) ([]byte, error) {
+				return []byte(`
+				[{
+					"identifier": "A",
+					"unitPrice": 50,
+					"specialPriceQuantity": 3,
+					"specialPrice": 130
+				}]`), nil
+			},
+				Unmarshal: func(b []byte, a any) error { return nil },
+			},
+			want:    []SKU{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.i.createPrices()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Handler.createPrices() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Handler.createPrices() = %v, want %v", got, tt.want)
 			}
 		})
 	}
